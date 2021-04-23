220cd818b706af9905369d87239d9b88f6c5dac2
Bartosz Balis <balis@agh.edu.pl>
2021-04-23 11:21:29 +0200

    Add asynchronous API and support for task agglomeration

--- main.ts
+++ main.ts
@@ -5,11 +5,13 @@ class SimpleScheduler {
     private wfJson; 
     private wfId; 
     private wflib;
+    private jobAgglomerations;
 
     constructor(config, wflib) {
         this.wfJson = config.wfJson;
         this.wfId = config.wfId;
         this.wflib = wflib;
+        this.jobAgglomerations = null; 
 
         // Here additional configuration could be read needed to
         // compute the schedule, e.g.:
@@ -30,7 +32,8 @@ class SimpleScheduler {
 
     /**
      * 
-     * Scheduler API method used to wait for permission to execute a task
+     * Scheduler API method used to synchronously wait for permission 
+     * to execute a task
      * 
      * @param wfId   - workflow identifier (integer 1..N)
      * @param procId - process identifier (integer 1..N) 
@@ -40,8 +43,56 @@ class SimpleScheduler {
     async getTaskExecutionPermission(wfId, procId: string) {
         return new Promise<string>((resolve, reject) => {
             console.log("Scheduler computing...");
-            setTimeout(() => resolve("testNodeName"), 2000);
+            setTimeout(() => resolve("testNodeName-sync"), 2000);
         });
+    }
+
+    /**
+     * addTaskItem
+     * 
+     * Scheduler API function which is an asynchronous alternative to
+     * 'getTaskExecutionPermission', at the same time allowing to
+     * execute tasks in groups (agglomeration).
+     * 
+     * @param taskItem - JSON object with the following structure:
+     * {
+     *   "ins": ins,
+     *   "outs": outs,
+     *   "context": context,
+     *   "cb": cb
+     * }
+     * where: 
+     * - 'ins', 'outs', 'context' and 'cb' are parameters of the task 
+     * function from which 'addJobItem' has been called.
+     * 
+     * @param taskFunctionCb is a function '(taskArray) => taskFunction(taskArray, node)'
+     * to be called asynchronously by the scheduler to execute the tasks
+     * passed as 'taskArray', where: 
+     * - each item in the 'taskArray' is a 'taskItem' 
+     * - 'node' is the name of the node assigned by the scheduler for the task (group)
+     * Example of such function is 'k8sCommandGroup':
+     * https://github.com/hyperflow-wms/hyperflow/blob/56f1f6e041e79b270753f66c0c07dd04bf7d00c5/functions/kubernetes/k8sCommand.js#L22
+     * 
+     * Allowing task arrays enables the scheduler to agglomerate tasks
+     * into groups. Configuration of task agglomeration (if any), is passed as 
+     * 'taskItem.context.appConfig.jobAgglomerations'
+     * (see https://github.com/hyperflow-wms/hyperflow/wiki/Task-agglomeration)
+     * (Note that for a given workflow it is sufficient to read this configuration
+     * only once, even though each task item will contain it.)
+     *      
+     */
+    addTaskItem(taskItem, taskFunctionCb) {
+        let wfId = taskItem.context.appId;
+        let procId = taskItem.context.procId;
+        let taskData = this.wfJson[procId-1];
+
+        if (!this.jobAgglomerations) {
+            this.jobAgglomerations = 
+                taskItem.context.appConfig.jobAgglomerations; // could be undefined
+        }
+
+        // Here the scheduler simply immediately invokes the callback to execute the task
+        return taskFunctionCb([taskItem], "testNodeName-async");
     }
 
     /**
@@ -54,7 +105,9 @@ class SimpleScheduler {
      * @param wfId - workflow identifier (integer 1..N)
      * @param procId - process identifier (integer 1..N)
      */
-    notifyTaskCompletion(wfId, procId) { }
+    notifyTaskCompletion(wfId, procId) { 
+        console.log("Task", procId, "in workflow", wfId, "has been finished.");
+    }
 }
 
 /**
